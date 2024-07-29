e7c007264c07c82b093fa92465bd6e0b01ea5a39
AlissonBltz <159453454+AlissonBltz@users.noreply.github.com>
2024-07-29 16:08:24 -0300

    Update UserService.java

--- src/main/java/me/dio/service/UserService.java
+++ src/main/java/me/dio/service/UserService.java
@@ -1,10 +1,11 @@
 package me.dio.service;
 
 import me.dio.domain.model.User;
-
+import java.util.List;
 public interface UserService {
 
     User findById(Long id);
-
+    List<User> findAll(User user);
     User create(User userToCreate);
+    ser deleteById(Long id);
 }
